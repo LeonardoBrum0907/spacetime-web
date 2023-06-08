aa928aec47adc9192df6d1237708d7584fb6cdd0
unknown <leonardobrum.dev@gmail.com>
2023-06-08 11:44:47 -0300

    initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="text-sm max-w-[140px] leading-snug">
+        {name}
+        <a
+          href="/api/auth/logout"
+          className="block text-[red] hover:text-[#ff6f6f]"
+        >
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
